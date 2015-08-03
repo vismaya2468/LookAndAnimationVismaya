ecb9c893947cd4244b02e134ef65186639f74497
vismaya2468 <vismayavijay92@gmail.com>
2015-08-03 12:39:00 +0530

    Commit message

---
+++ gen/com/fyreflycorp/vismaya/lookandanimationvismaya/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fyreflycorp.vismaya.lookandanimationvismaya;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
